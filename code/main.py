8bae55695d2eda21771bc52d4bed829ea2b0ac62
mvinza34 <mvinza34@gmail.com>
2024-10-13 17:21:38 -0400

    Update main.py with boilerplate code
    - Replaced Player class with boilerplate code
    - Imported Player class from player.py
    - Main class wil organize core functionality

--- code/main.py
+++ code/main.py
@@ -1,23 +1,10 @@
-class Player:
-    def __init__(self, name, starting_souls=1000):
-        
-        self.name = name
-        self.souls = starting_souls
+from player import Player
 
-        # Define the attributes with initial levels
-        self.attributes = {
-            "Vitality": 1,
-            "Attunment": 1,
-            "Endurance": 1,
-            "Strength": 1,
-            "Dexterity": 1,
-            "Resistance": 1, 
-            "Intelligence": 1,
-            "Faith": 1
-            }
+class Main:
+    def __init__(self):
+        pass
 
-        self.base_cost = 100 # Base cost for leveling up any attribute
-
-
+if __name__== '__main__':
+    main = Main()
 
 
