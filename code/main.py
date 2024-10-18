93850c2a88856922bfc6bc687a6df7e50f40bc8c
mvinza34 <mvinza34@gmail.com>
2024-10-17 23:17:02 -0400

    Update main.py with new functions
    - Added menu function to display options for the user to choose
    - Added run function to run the soul budget tracker

--- code/main.py
+++ code/main.py
@@ -2,9 +2,16 @@ from player import Player
 
 class Main:
     def __init__(self):
-        pass
+        self.player = Player("Marlon",total_souls=1000) # Start with 1000 souls
+        
+    def menu(self):
+        print("\n--- SoulKeeper ---")
+
+    def run(self):
+        self.menu()
 
 if __name__== '__main__':
     main = Main()
+    main.run()
 
 
