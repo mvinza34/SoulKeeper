3a1bccb3f744614951061115ed983e8fea606d89
mvinza34 <mvinza34@gmail.com>
2024-10-31 23:54:11 -0400

    Update player.py and main.py with class checking requirement
    - Relocated the logic for choosing a class from level_up function to new choose_class function
    - Added check_for class variable to check if the user chose a starting class
    - Removed outdated get_total_level function
    - Revised menu and run functions in main.py to include choosing a class and checking for a class
    - Improves functionality as the user cannot level up if no class has been chosen

--- code/main.py
+++ code/main.py
@@ -5,38 +5,46 @@ class Main:
         self.player = Player("Marlon",total_souls=3000) # Start with 3000 souls
         
     def menu(self):
-        print("\n--- SoulKeeper ---")
-        print("1. Level up attribute")
-        print("2. View current stats")
-        print("3. Save progress")
-        print("4. Load progress")
-        print("5. Delete progress")
-        print("6. Exit")
+        print("1. Choose class")
+        print("2. Level up attribute")
+        print("3. View current stats")
+        print("4. Save progress")
+        print("5. Load progress")
+        print("6. Delete progress")
+        print("7. Exit")
 
     def run(self):
+        print("\n--- SoulKeeper ---")
+        print("Welcome, Chosen Undead!")
+
         while True: 
             self.menu()
 
             self.choice = input("Choose an option: ")
 
             if self.choice == '1':
-                self.player.level_up()
+                self.player.choose_class()
             elif self.choice == '2':
-                self.player.show_status()
+                if self.player.check_for_class == True:
+                    self.player.level_up()
+                else:
+                    print("You must first choose a class before leveling up!")
             elif self.choice == '3':
-                pass
-                #self.player.save_progress()
+                self.player.show_status()
             elif self.choice == '4':
                 pass
-                #self.player.load_progress()
+                #self.player.save_progress()
             elif self.choice == '5':
+                pass
+                #self.player.load_progress()
+            elif self.choice == '6':
                 pass
                 # erase = input("Do you wish to start all over again, Chosen Undead? Enter 'Y' for yes or any key for no. ")
                 # if erase == "Y":
                 #     self.player.delete_progress()
                 # else:
                 #     print("Progress kept!")
-            elif self.choice == '6':
+            elif self.choice == '7':
                 print("Farewell, Chosen Undead! Don't you dare go hollow!")
                 break
             else:
--- code/player.py
+++ code/player.py
@@ -9,17 +9,20 @@ class Player:
 
         # Define the classes with starting level and attributes
         self.classes = {
-            "Warrior": {"Level": 4, "Vitality": 11, "Attunment": 8, "Endurance": 12, "Strength": 13, "Dexterity": 13, "Resistance": 11,  "Intelligence": 9, "Faith": 9},
-            "Knight": {"Level": 5, "Vitality": 14, "Attunment": 10, "Endurance": 10, "Strength": 11, "Dexterity": 11, "Resistance": 10,  "Intelligence": 9, "Faith": 11},
-            "Wanderer": {"Level": 3, "Vitality": 10, "Attunment": 11, "Endurance": 10, "Strength": 10, "Dexterity": 14, "Resistance": 12,  "Intelligence": 11, "Faith": 8},
-            "Thief": {"Level": 5, "Vitality": 9, "Attunment": 11, "Endurance": 9, "Strength": 9, "Dexterity": 15, "Resistance": 10,  "Intelligence": 12, "Faith": 11},
-            "Bandit": {"Level": 4, "Vitality": 12, "Attunment": 8, "Endurance": 14, "Strength": 14, "Dexterity": 9, "Resistance": 11,  "Intelligence": 8, "Faith": 10},
-            "Hunter": {"Level": 4, "Vitality": 11, "Attunment": 9, "Endurance": 11, "Strength": 12, "Dexterity": 14, "Resistance": 11,  "Intelligence": 9, "Faith": 9},
-            "Sorcerer": {"Level": 3, "Vitality": 8, "Attunment": 15, "Endurance": 8, "Strength": 9, "Dexterity": 11, "Resistance": 8,  "Intelligence": 15, "Faith": 8},
-            "Pyromancer": {"Level": 1, "Vitality": 10, "Attunment": 12, "Endurance": 11, "Strength": 12, "Dexterity": 9, "Resistance": 12,  "Intelligence": 10, "Faith": 8},
-            "Cleric": {"Level": 2, "Vitality": 11, "Attunment": 11, "Endurance": 9, "Strength": 12, "Dexterity": 8, "Resistance": 11,  "Intelligence": 8, "Faith": 14},
-            "Deprived": {"Level": 6, "Vitality": 11, "Attunment": 11, "Endurance": 11, "Strength": 11, "Dexterity": 11, "Resistance": 11,  "Intelligence": 11, "Faith": 11}
+            "Warrior": {"Level": 4, "Vitality": 11, "Attunement": 8, "Endurance": 12, "Strength": 13, "Dexterity": 13, "Resistance": 11,  "Intelligence": 9, "Faith": 9},
+            "Knight": {"Level": 5, "Vitality": 14, "Attunement": 10, "Endurance": 10, "Strength": 11, "Dexterity": 11, "Resistance": 10,  "Intelligence": 9, "Faith": 11},
+            "Wanderer": {"Level": 3, "Vitality": 10, "Attunement": 11, "Endurance": 10, "Strength": 10, "Dexterity": 14, "Resistance": 12,  "Intelligence": 11, "Faith": 8},
+            "Thief": {"Level": 5, "Vitality": 9, "Attunement": 11, "Endurance": 9, "Strength": 9, "Dexterity": 15, "Resistance": 10,  "Intelligence": 12, "Faith": 11},
+            "Bandit": {"Level": 4, "Vitality": 12, "Attunement": 8, "Endurance": 14, "Strength": 14, "Dexterity": 9, "Resistance": 11,  "Intelligence": 8, "Faith": 10},
+            "Hunter": {"Level": 4, "Vitality": 11, "Attunement": 9, "Endurance": 11, "Strength": 12, "Dexterity": 14, "Resistance": 11,  "Intelligence": 9, "Faith": 9},
+            "Sorcerer": {"Level": 3, "Vitality": 8, "Attunement": 15, "Endurance": 8, "Strength": 9, "Dexterity": 11, "Resistance": 8,  "Intelligence": 15, "Faith": 8},
+            "Pyromancer": {"Level": 1, "Vitality": 10, "Attunement": 12, "Endurance": 11, "Strength": 12, "Dexterity": 9, "Resistance": 12,  "Intelligence": 10, "Faith": 8},
+            "Cleric": {"Level": 2, "Vitality": 11, "Attunement": 11, "Endurance": 9, "Strength": 12, "Dexterity": 8, "Resistance": 11,  "Intelligence": 8, "Faith": 14},
+            "Deprived": {"Level": 6, "Vitality": 11, "Attunement": 11, "Endurance": 11, "Strength": 11, "Dexterity": 11, "Resistance": 11,  "Intelligence": 11, "Faith": 11}
         }
+
+        # Checks if the user chose a starting class
+        self.check_for_class = False
 
     def increase_attribute_level(self, selected_class, attribute, base_cost=50, scaling_factor=1.2):
         # Get the current level of the attribute in the chosen class
@@ -40,31 +43,30 @@ class Player:
         else:
             print("Not enough souls!")
 
-    def level_up(self):
+    def choose_class(self):
         self.starting_class_name = input("Choose a starting class: ").strip()
         if self.starting_class_name in self.classes:
             print(f"You chose to start as a {self.starting_class_name}.")
             self.selected_class = self.classes[self.starting_class_name]
             print(f"Your current soul level is {self.selected_class['Level']}.")
+            self.check_for_class = True
+        else:
+            print("Invalid class!")
 
-        self.attribute = input("Enter an attribute to level up (Vitality, Attunment, Endurance, Strength, Dexterity, Resistance, Intelligence, Faith): ")
+    def level_up(self):
+        self.attribute = input("Enter an attribute to level up (Vitality, Attunement, Endurance, Strength, Dexterity, Resistance, Intelligence, Faith): ")
         if self.attribute in self.selected_class:
             self.increase_attribute_level(self.selected_class, self.attribute)
         else:
             print("Invaild attribute!")
 
     def show_status(self):
-        # Display the class, levels, atrributes, and current souls
+        # Display the class, current level, current atrributes, and current souls
         print(f"\n{self.name}'s current status:")
         print(f"Class: {self.starting_class_name}")
         for stat, value in self.selected_class.items():
             print(f"{stat}: {value}")
         print(f"Souls: {self.total_souls}")
-
-
-    # def get_total_level(self):
-    #     # Show the overall level of the player
-    #     return sum(self.attributes.values())
 
     # def save_progress(self, filename="save.json"):
     #     data = {
