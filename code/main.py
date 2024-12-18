89ce7f0e758204cfd81118b4660a763be9d2c480
mvinza34 <mvinza34@gmail.com>
2024-12-17 19:44:50 -0500

    Update main.py with new Rich library
    - Used the Rich library to add colors and emojis to the interface
    - Improves presentation as it makes the interface more appealing to users

--- code/main.py
+++ code/main.py
@@ -1,22 +1,25 @@
+from rich.console import Console
 from player import Player
+
+console = Console()
 
 class Main:
     def __init__(self):
-        print("\n------ SoulKeeper ------\n")
+        console.print("\n------ :fire: SoulKeeper :fire: ------\n")
         self.start = True
         self.access = False
 
     def start_app(self):
         # Prompt the user to enter a valid name and starting number of souls before proceeding
         while self.start:
-            self.player_name = input("Welcome, Chosen Undead! Please enter your name: ").strip()
+            self.player_name = console.input("Greetings, Chosen Undead :skull:! Please enter your name: ").strip()
 
             if not self.player_name:
                 print("You must enter a name.\n")
                 continue
 
             try:
-                self.player_souls = int(input("Enter how many souls you have: "))
+                self.player_souls = int(console.input("Enter how many souls :fire: you have: "))
                 if self.player_souls < 0:
                     print("Souls must be a positive integer.\n")
                     continue
@@ -28,19 +31,19 @@ class Main:
         # Grants user access to main menu 
         self.access = True
         self.player = Player(self.player_name, self.player_souls)
-        print(f"Welcome, {self.player_name}, to SoulKeeper! You start with {self.player_souls} souls!\n")
+        console.print(f"Welcome, {self.player_name}, to SoulKeeper! You start with {self.player_souls} souls :fire:!\n")
         self.choices()
 
     def menu(self):
-        print("1. Choose class")
-        print("2. Level up attribute")
-        print("3. View current stats")
-        print("4. Add more souls")
-        print("5. Show total souls spent")
-        print("6. Save progress")
-        print("7. Load progress")
-        print("8. Delete progress")
-        print("9. Exit\n")
+        console.print("[dodger_blue1]1. Choose class :crossed_swords:")
+        console.print("[gold1]2. Level up attribute :muscle:")
+        console.print("[white]3. View current stats :scroll:")
+        console.print("[turquoise2]4. Add more souls :heavy_plus_sign::fire:")
+        console.print("[magenta]5. Show total souls spent :fire:")
+        console.print("[bright_green]6. Save progress :file_folder:")
+        console.print("[cyan]7. Load progress :unlock:")
+        console.print("[bright_yellow]8. Delete progress :wastebasket:")
+        console.print("[bright_red]9. Exit :coffin:\n")
 
     def menu_choice_1(self):
         self.player.choose_class()
@@ -58,7 +61,7 @@ class Main:
             print("You must first choose a class before displaying your current status!\n")
 
     def menu_choice_4(self):
-        self.more_souls = int(input("Enter how many souls you wish to add: "))
+        self.more_souls = int(console.input("Enter how many souls :fire: you wish to add: "))
         self.player.add_souls(self.more_souls)
 
     def menu_choice_5(self):
@@ -81,7 +84,7 @@ class Main:
         #     print("Progress kept!")
 
     def menu_choice_9(self):
-        print(f"Farewell, {self.player_name}! Don't you dare go hollow!\n")
+        console.print(f"Farewell, {self.player_name}! Don't you dare go hollow :skull:!\n")
         self.access = False
 
     def choices(self):
