9f8a57a246fb89305471debba963241af68b293f
mvinza34 <mvinza34@gmail.com>
2024-12-19 00:49:28 -0500

    Update player.py with new cost reduction perk features
    - Revised classes dictionary to include cost reduction perks
    - Revised cost variable in increase_attribute_level function to include cost reduction perks
    - Revised show_status function to create new table for perks
    - Enhances functionality as the user can now take perks into account when choosing a starting class

--- code/player.py
+++ code/player.py
@@ -15,16 +15,46 @@ class Player:
 
         # Define the classes with starting level and attributes
         self.classes = {
-            "Warrior": {"Level": 4, "Vitality": 11, "Attunement": 8, "Endurance": 12, "Strength": 13, "Dexterity": 13, "Resistance": 11,  "Intelligence": 9, "Faith": 9},
-            "Knight": {"Level": 5, "Vitality": 14, "Attunement": 10, "Endurance": 10, "Strength": 11, "Dexterity": 11, "Resistance": 10,  "Intelligence": 9, "Faith": 11},
-            "Wanderer": {"Level": 3, "Vitality": 10, "Attunement": 11, "Endurance": 10, "Strength": 10, "Dexterity": 14, "Resistance": 12,  "Intelligence": 11, "Faith": 8},
-            "Thief": {"Level": 5, "Vitality": 9, "Attunement": 11, "Endurance": 9, "Strength": 9, "Dexterity": 15, "Resistance": 10,  "Intelligence": 12, "Faith": 11},
-            "Bandit": {"Level": 4, "Vitality": 12, "Attunement": 8, "Endurance": 14, "Strength": 14, "Dexterity": 9, "Resistance": 11,  "Intelligence": 8, "Faith": 10},
-            "Hunter": {"Level": 4, "Vitality": 11, "Attunement": 9, "Endurance": 11, "Strength": 12, "Dexterity": 14, "Resistance": 11,  "Intelligence": 9, "Faith": 9},
-            "Sorcerer": {"Level": 3, "Vitality": 8, "Attunement": 15, "Endurance": 8, "Strength": 9, "Dexterity": 11, "Resistance": 8,  "Intelligence": 15, "Faith": 8},
-            "Pyromancer": {"Level": 1, "Vitality": 10, "Attunement": 12, "Endurance": 11, "Strength": 12, "Dexterity": 9, "Resistance": 12,  "Intelligence": 10, "Faith": 8},
-            "Cleric": {"Level": 2, "Vitality": 11, "Attunement": 11, "Endurance": 9, "Strength": 12, "Dexterity": 8, "Resistance": 11,  "Intelligence": 8, "Faith": 14},
-            "Deprived": {"Level": 6, "Vitality": 11, "Attunement": 11, "Endurance": 11, "Strength": 11, "Dexterity": 11, "Resistance": 11,  "Intelligence": 11, "Faith": 11}
+            "Warrior": {"Level": 4,
+                       "Vitality": 11, "Attunement": 8, "Endurance": 12, "Strength": 13, "Dexterity": 13, "Resistance": 11,  "Intelligence": 9, "Faith": 9,
+                        "Perks": {"Strength Cost Reduction": 0.6, "Dexterity Cost Reduction": 0.6}
+                        },
+            "Knight": {"Level": 5, 
+                       "Vitality": 14, "Attunement": 10, "Endurance": 10, "Strength": 11, "Dexterity": 11, "Resistance": 10,  "Intelligence": 9, "Faith": 11,
+                       "Perks": {"Strength Cost Reduction": 0.4, "Dexterity Cost Reduction": 0.4}
+                       },
+            "Wanderer": {"Level": 3, 
+                         "Vitality": 10, "Attunement": 11, "Endurance": 10, "Strength": 10, "Dexterity": 14, "Resistance": 12,  "Intelligence": 11, "Faith": 8,
+                         "Perks": {"Strength Cost Reduction": 0.7, "Dexterity Cost Reduction": 0.5}
+                         },
+            "Thief": {"Level": 5, 
+                      "Vitality": 9, "Attunement": 11, "Endurance": 9, "Strength": 9, "Dexterity": 15, "Resistance": 10,  "Intelligence": 12, "Faith": 11,
+                      "Perks": {"Strength Cost Reduction": 0.8, "Dexterity Cost Reduction": 0.4}
+                      },
+            "Bandit": {"Level": 4, 
+                       "Vitality": 12, "Attunement": 8, "Endurance": 14, "Strength": 14, "Dexterity": 9, "Resistance": 11,  "Intelligence": 8, "Faith": 10,
+                       "Perks": {"Strength Cost Reduction": 0.4, "Dexterity Cost Reduction": 0.8}
+                       },
+            "Hunter": {"Level": 4, 
+                       "Vitality": 11, "Attunement": 9, "Endurance": 11, "Strength": 12, "Dexterity": 14, "Resistance": 11,  "Intelligence": 9, "Faith": 9,
+                       "Perks": {"Strength Cost Reduction": 0.7, "Dexterity Cost Reduction": 0.7}
+                       },
+            "Sorcerer": {"Level": 3, 
+                         "Vitality": 8, "Attunement": 15, "Endurance": 8, "Strength": 9, "Dexterity": 11, "Resistance": 8,  "Intelligence": 15, "Faith": 8,
+                         "Perks": {"Intelligence Cost Reduction": 0.4, "Dexterity Cost Reduction": 0.6}
+                         },
+            "Pyromancer": {"Level": 1, 
+                           "Vitality": 10, "Attunement": 12, "Endurance": 11, "Strength": 12, "Dexterity": 9, "Resistance": 12,  "Intelligence": 10, "Faith": 8,
+                           "Perks": {"Intelligence Cost Reduction": 0.5, "Faith Cost Reduction": 0.5}
+                           },
+            "Cleric": {"Level": 2, 
+                       "Vitality": 11, "Attunement": 11, "Endurance": 9, "Strength": 12, "Dexterity": 8, "Resistance": 11,  "Intelligence": 8, "Faith": 14,
+                       "Perks": {"Faith Cost Reduction": 0.4, "Strength Cost Reduction": 0.6}
+                       },
+            "Deprived": {"Level": 6, 
+                         "Vitality": 11, "Attunement": 11, "Endurance": 11, "Strength": 11, "Dexterity": 11, "Resistance": 11,  "Intelligence": 11, "Faith": 11,
+                         "Perks": {"Strength Cost Reduction": 0.8, "Dexterity Cost Reduction": 0.8}
+                         }
         }
 
         # Checks if the user chose a starting class
@@ -45,8 +75,10 @@ class Player:
         # Get the current level of the attribute in the chosen class
         current_level = selected_class[attribute]
 
+        cost_multiplier = self.selected_class.get("Perks", {}).get(f"{attribute} Cost Reduction", 1.0)
+
         # Calculate how many souls it costs to level up
-        cost = int(base_cost * (current_level ** scaling_factor))
+        cost = int(base_cost * (current_level ** scaling_factor) * cost_multiplier)
 
         # Spend souls to level up the desired attribute
         if self.total_souls >= cost:
@@ -86,10 +118,23 @@ class Player:
         status_table.add_row("Class", self.starting_class_name)
         
         for stat, value in self.selected_class.items():
-            status_table.add_row(stat, str(value))
+            if stat != "Perks":
+                status_table.add_row(stat, str(value))
 
         status_table.add_row("Souls", str(self.total_souls))
+         
         console.print(status_table)
+
+        # Display the perks depending on the class
+        if "Perks" in self.selected_class:
+            perks_table = Table(title="Perks", show_header=True, header_style="bold magenta")
+            perks_table.add_column("Perk", style="green")
+            perks_table.add_column("Value", justify="right")
+    
+        for perk, value in self.selected_class["Perks"].items():
+            perks_table.add_row(perk, str(value))
+
+        console.print(perks_table)
 
     def show_souls_spent(self):
         console.print(f"Total Souls Spent: {self.total_souls_spent} :fire:\n")
